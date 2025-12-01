f75cda0b150e2532508f22e9d5efd32f04b97d78
Vladislav Yemelyanov <vladislav.yemelyanov.93@gmail.com>
2025-12-01 02:05:14 +0200

    🐥 make short example

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:tovi/tovi.dart';
 
+/// Tovi Example
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                autofocus: true,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                inputFormatters: [Tovi()],
-              ),
-            ],
+  runApp(
+    MaterialApp(
+      title: 'Tovi Demo',
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  autofocus: true,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  inputFormatters: [Tovi()],
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
